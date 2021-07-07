9aa06075a545009eb165cc75032c0d1ff3d591d0
rubengerritsen <r.h.j.gerritsen@gmail.com>
2021-07-07 10:34:49 +0200

    update

--- rgpytools/crystal.py
+++ rgpytools/crystal.py
@@ -1,8 +1,36 @@
 
 import numpy as np
 
-def fromCos(A: float, B: float, C: float, cosAB: float, cosAC: float, cosBC: float) -> float:
+def ucMatFromCos(A: float, B: float, C: float, cosAB: float, cosAC: float, cosBC: float) -> float:
+  """ Returns unit cell matrix of triclinic box. """
   a_vec = 6.277 * np.array([1,0,0])
   b_vec = 7.663 * np.array([cosAB, np.sqrt(1- cosAB**2), 0 ])
   c_vec = 14.363 * np.array([cosAC, (cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2), np.sqrt(1- cosAC**2 - ((cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2))**2)])
   return np.stack([a_vec, b_vec, c_vec])
+
+def ucMatFromAngles(A: float, B: float, C: float, alpha: float, beta: float, gamma: float) -> float:
+  """ Returns unit cell matrix of triclinic box. """
+  cosAB = np.cos(gamma)
+  cosAC = np.cos(beta)
+  cosBC = np.cos(alpha)
+  a_vec = 6.277 * np.array([1,0,0])
+  b_vec = 7.663 * np.array([cosAB, np.sqrt(1- cosAB**2), 0 ])
+  c_vec = 14.363 * np.array([cosAC, (cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2), np.sqrt(1- cosAC**2 - ((cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2))**2)])
+  return np.stack([a_vec, b_vec, c_vec])
+
+def ucVecFromCos(A: float, B: float, C: float, cosAB: float, cosAC: float, cosBC: float) -> float:
+  """ Returns unit cell matrix of triclinic box. """
+  a_vec = 6.277 * np.array([1,0,0])
+  b_vec = 7.663 * np.array([cosAB, np.sqrt(1- cosAB**2), 0 ])
+  c_vec = 14.363 * np.array([cosAC, (cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2), np.sqrt(1- cosAC**2 - ((cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2))**2)])
+  return (a_vec, b_vec, c_vec)
+
+def ucVecFromAngles(A: float, B: float, C: float, alpha: float, beta: float, gamma: float) -> float:
+  """ Returns unit cell matrix of triclinic box. """
+  cosAB = np.cos(np.deg2rad(gamma))
+  cosAC = np.cos(np.deg2rad(beta))
+  cosBC = np.cos(np.deg2rad(alpha))
+  a_vec = 6.277 * np.array([1,0,0])
+  b_vec = 7.663 * np.array([cosAB, np.sqrt(1- cosAB**2), 0 ])
+  c_vec = 14.363 * np.array([cosAC, (cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2), np.sqrt(1- cosAC**2 - ((cosBC- cosAC*cosAB)/np.sqrt(1-cosAB**2))**2)])
+  return (a_vec, b_vec, c_vec)
